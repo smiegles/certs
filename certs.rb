06e84165b6c56b7edb66138fe2f56e3e13946416
Olivier Beg <olivier@hotmail.lv>
2018-06-13 15:18:36 +0200

    set comma seperated file

--- certs.rb
+++ certs.rb
@@ -4,5 +4,5 @@ require_relative './lib/certs'
 begin
   importer = Certs::Import.new(ARGV[0])
   importer.run
-  importer.domains.each { |domain| puts domain }
+  importer.domains.each { |domain| puts ARGV[1] + "," + domain }
 rescue; end # When importing you don't want to have a rescue in your logs.
